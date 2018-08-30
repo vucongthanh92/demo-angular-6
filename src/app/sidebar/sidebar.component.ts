9129d7bdcaaf7b26fbe3ca5bef4158902f052765
thanhvc <vucongthanh92@gmail.com>
2018-08-30 13:45:59 +0700

    angular animation

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -15,7 +15,6 @@ export class SidebarComponent implements OnInit {
       (_: NavigationEnd) => {
         if (_.url !== undefined) {
           this.currentUrl = _.url;
-          console.log(this.currentUrl);
         }
       }
     );
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-import { Observable } from 'rxjs';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transition: 'translateY(-15px)' }),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
 export class UsersComponent implements OnInit {
 
@@ -20,5 +44,4 @@ export class UsersComponent implements OnInit {
       }
     );
   }
-
 }
